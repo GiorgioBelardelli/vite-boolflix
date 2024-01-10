2bec74732ba21c78ca4166fa9c8cd5525593a3e3
GiorgioBelardelli <asterapocalypse@gmail.com>
2024-01-10 13:35:39 +0100

    config: AppHeader

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script>
 import axios from 'axios'
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue'
 
 import { store } from './store';
 
 export default {
   components: {
-
+    AppHeader
   },
   data() {
     return {
@@ -17,11 +17,19 @@ export default {
 </script>
 
 <template>
-  <h1>START</h1>
+  <AppHeader message="BOOLFLIX" />
+  <main>
+    <h1>FILMLIST GOES HERE</h1>
+  </main>
 </template>
 
 
 <style lang="scss">
 @use './styles/general.scss' as *;
 @use './styles/partials/variables.scss' as *;
+
+main {
+  background-color: $grey;
+  padding: 2rem;
+}
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,58 @@
+<script>
+export default {
+    name: 'AppHeader',
+    props: ['message'],
+}
+</script>
+
+<template>
+    <header>
+        <div class="container">
+            <h1>{{ message }}</h1>
+
+            <div id="search-container">
+                <input class="text-input" type="text" placeholder="Cerca un film...">
+                <input class="search-button" type="button" value="Cerca">
+            </div>
+
+        </div>
+
+    </header>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/general.scss' as *;
+@use '../styles/partials/variables.scss' as *;
+
+header {
+    background-color: $black;
+    padding: 1rem;
+
+    .container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    h1 {
+        color: $red;
+    }
+
+    input {
+        padding: .5rem .7rem;
+        font-size: 15px;
+        border-radius: 5px;
+        border: 0px;
+        margin: 0 .5rem;
+    }
+
+    .text-input {
+        border: 2px solid $grey;
+    }
+
+    .search-button {
+        background-color: $red;
+        cursor: pointer;
+    }
+}
+</style>
